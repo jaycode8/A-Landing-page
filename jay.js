774c1cf1a6364662fc26eabc71960019fd8db5f8
JayMoh <86493008+jaycode8@users.noreply.github.com>
2022-05-20 15:45:53 +0300

    Add files via upload

---
+++ jay.js
@@ -0,0 +1,8 @@
+const navbarShow =()=>{
+    // document.getElementById('nav').classList.toggle('active')
+    alert('hello')
+} 
+function jay(){
+    // alert('jay on the move')
+    document.getElementById('nav').classList.toggle('active')
+}
